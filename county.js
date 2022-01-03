e475d6987022e5707087f46c672135abd6c4a33c
ShubhanYenuganti <63825093+ShubhanYenuganti@users.noreply.github.com>
2022-01-02 21:58:40 -0800

    Add files via upload

---
+++ county.js
@@ -0,0 +1,53 @@
+document.getElementById('form').onsubmit = function () {
+    getStats(document.getElementById('city').value);
+};
+
+const getStats = (city) => {
+    fetch("https://corona.lmao.ninja/v2/jhucsse/counties/" + city, requestOptions)
+        .then(response => response.text())
+        .then(
+            result => updateText(result)
+        )
+        .catch(
+            error =>
+            document.getElementById('output').value = "API request failed or you didn't enter a valid input"
+        );
+};
+
+const updateText = (data) => {
+    console.log(data);
+    var info = JSON.parse(data);
+    var city = "For: " + info[0].county + "\n \n";
+    var updateAt = "Information is accurate as of: " + "\n" + info[0].updatedAt + "\n \n";
+    var confirm = info[0].stats.confirmed + " confirmed cases \n";
+    var death = info[0].stats.deaths + " deaths \n";
+    var recover = info[0].stats.recovered + " recovered \n"
+
+    if (city == null) {
+        document.getElementById('output').value = "Enter valid county";
+    } else {
+        document.getElementById('output').value = city + updateAt + confirm + death + recover;
+    }
+
+}
+
+var form = document.getElementById("form");
+
+function handleForm(event) {
+    event.preventDefault();
+}
+form.addEventListener('submit', handleForm);
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
